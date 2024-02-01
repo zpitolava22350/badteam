78b95a2c84c8ea1f6b3d53cc15131d26001af4c0
zpitolava22350 <74846224+zpitolava22350@users.noreply.github.com>
2024-01-31 20:46:11 -0700

    bruh

---
+++ PictureBoxTest/PictureBoxTest/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace PictureBoxTest {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.canvasPanel = new System.Windows.Forms.PictureBox();
+            this.gameRefresh = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvasPanel
+            // 
+            this.canvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvasPanel.Location = new System.Drawing.Point(0, 0);
+            this.canvasPanel.Name = "canvasPanel";
+            this.canvasPanel.Size = new System.Drawing.Size(800, 450);
+            this.canvasPanel.TabIndex = 0;
+            this.canvasPanel.TabStop = false;
+            this.canvasPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // gameRefresh
+            // 
+            this.gameRefresh.Enabled = true;
+            this.gameRefresh.Interval = 1;
+            this.gameRefresh.Tick += new System.EventHandler(this.gameRefresh_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.canvasPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPanel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvasPanel;
+        private System.Windows.Forms.Timer gameRefresh;
+    }
+}
+
---
+++ PictureBoxTest/PictureBoxTest/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxTest {
+
+    public partial class Form1: Form {
+
+        private Pen drawingPen = new Pen(Color.Black, 2);
+        private Bitmap canvas;
+        int mouseX = 0;
+        int mouseY = 0;
+
+        private bool isAKeyDown = false;
+        private bool isDKeyDown = false;
+
+        Person wall = new Person(400,300,600,100);
+
+        int counter = 0;
+        //private Bitmap clearedCanvas;
+
+        public Form1() {
+            InitializeComponent();
+            //clearedCanvas = new Bitmap(canvasPanel.Width, canvasPanel.Height);
+            canvas = new Bitmap(canvasPanel.Width, canvasPanel.Height);
+            canvasPanel.BackgroundImage = canvas;
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+
+        private void gameRefresh_Tick(object sender, EventArgs e) {
+
+            SharedVariablesClass.playerYVel += 0.1;
+
+            if (isAKeyDown) {
+                SharedVariablesClass.playerXVel -= 0.1;
+            }
+            if (isDKeyDown) {
+                SharedVariablesClass.playerXVel += 0.1;
+            }
+
+            wall.SayHello();
+
+            SharedVariablesClass.playerX += SharedVariablesClass.playerXVel;
+            SharedVariablesClass.playerY += SharedVariablesClass.playerYVel;
+            using (Graphics g = Graphics.FromImage(canvas)) {
+                g.Clear(Color.White);
+                g.DrawRectangle(drawingPen, (int)SharedVariablesClass.playerX - 10, (int)SharedVariablesClass.playerY - 10, 20, 20);
+                g.DrawRectangle(drawingPen, (int)(wall.x-(wall.dx/2)), (int)(wall.y - (wall.dy / 2)), (int)wall.dx, (int)wall.dy);
+            }
+            canvasPanel.Invalidate();
+            Console.WriteLine($"{++counter}");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.A) {
+                isAKeyDown = true;
+            } else if (e.KeyCode == Keys.D) {
+                isDKeyDown = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.A) {
+                isAKeyDown = false;
+            } else if (e.KeyCode == Keys.D) {
+                isDKeyDown = false;
+            }
+        }
+    }
+
+    public class SharedVariablesClass {
+        public static double playerX = 400;
+        public static double playerY = 200;
+        public static double playerXVel = 0;
+        public static double playerYVel = 0;
+    }
+
+    public class Person {
+        // Properties
+        public double x { get; set; }
+        public double y { get; set; }
+        public double dx { get; set; }
+        public double dy { get; set; }
+
+        // Constructor
+        public Person(double x, double y, double dx, double dy) {
+            this.x = x;
+            this.y = y;
+            this.dx = dx;
+            this.dy = dy;
+        }
+
+        // Methods
+        public void SayHello() {
+            if ((SharedVariablesClass.playerY + 10) <= this.y - (this.dy / 2) &&
+                (SharedVariablesClass.playerY + 10) + SharedVariablesClass.playerYVel > this.y - (this.dy / 2)) {
+                SharedVariablesClass.playerY = this.y - (this.dy / 2) - 10;
+                SharedVariablesClass.playerYVel = 0;
+            }
+        }
+    }
+
+}
