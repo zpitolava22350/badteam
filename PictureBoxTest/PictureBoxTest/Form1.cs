ab892b57ac24105edab064644a7d860dd758dbc5
zpitolava22350 <74846224+zpitolava22350@users.noreply.github.com>
2024-02-01 21:52:16 -0700

    tf

--- PictureBoxTest/PictureBoxTest/Form1.cs
+++ PictureBoxTest/PictureBoxTest/Form1.cs
@@ -15,24 +15,36 @@ namespace PictureBoxTest {
 
     public partial class Form1: Form {
 
-        private Pen drawingPen = new Pen(Color.Black, 2);
+        private Pen drawingPen = new Pen(Color.Black, 1);
+        SolidBrush shadowBrush = new SolidBrush(Color.Black);
         private Bitmap canvas;
+        Random random = new Random();
+
+        const int pixelSize = 1;
+
         int mouseX = 0;
         int mouseY = 0;
+
+        int pixelColor;
+
+        FastNoiseLite noise = new FastNoiseLite();
 
         private bool isAKeyDown = false;
         private bool isDKeyDown = false;
 
-        Person wall = new Person(400,300,600,100);
+        //Person wall = new Person(400,300,600,100);
 
         int counter = 0;
         //private Bitmap clearedCanvas;
 
         public Form1() {
+
+            noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2S);
             InitializeComponent();
             //clearedCanvas = new Bitmap(canvasPanel.Width, canvasPanel.Height);
             canvas = new Bitmap(canvasPanel.Width, canvasPanel.Height);
             canvasPanel.BackgroundImage = canvas;
+
         }
 
 
@@ -43,6 +55,7 @@ namespace PictureBoxTest {
 
         private void gameRefresh_Tick(object sender, EventArgs e) {
 
+            /*
             SharedVariablesClass.playerYVel += 0.1;
 
             if (isAKeyDown) {
@@ -56,10 +69,25 @@ namespace PictureBoxTest {
 
             SharedVariablesClass.playerX += SharedVariablesClass.playerXVel;
             SharedVariablesClass.playerY += SharedVariablesClass.playerYVel;
+            */
+
             using (Graphics g = Graphics.FromImage(canvas)) {
+                g.Clear(Color.White);
+
+                pixelColor = 0;
+
+                for (int x = 0; x < canvas.Width; x+=pixelSize) {
+                    for(int y = 0; y < canvas.Height; y+=pixelSize) {
+                        pixelColor = (int)Math.Floor(Math.Abs(noise.GetNoise(x-mouseX, y-mouseY)) * 250);
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(pixelColor, pixelColor, pixelColor)), x, y, pixelSize, pixelSize); 
+                    }
+                }
+
+                /*
                 g.Clear(Color.White);
                 g.DrawRectangle(drawingPen, (int)SharedVariablesClass.playerX - 10, (int)SharedVariablesClass.playerY - 10, 20, 20);
                 g.DrawRectangle(drawingPen, (int)(wall.x-(wall.dx/2)), (int)(wall.y - (wall.dy / 2)), (int)wall.dx, (int)wall.dy);
+                */
             }
             canvasPanel.Invalidate();
             Console.WriteLine($"{++counter}");
@@ -89,6 +117,7 @@ namespace PictureBoxTest {
         public static double playerYVel = 0;
     }
 
+    /*
     public class Person {
         // Properties
         public double x { get; set; }
@@ -113,5 +142,6 @@ namespace PictureBoxTest {
             }
         }
     }
+    */
 
 }
