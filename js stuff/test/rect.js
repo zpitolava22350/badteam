04111962282cfdf42026922eac2c852601e5482a
zpitolava22350 <zpitolava22350@gmail.com>
2024-05-05 02:56:57 -0600

    bruh

---
+++ js stuff/test/rect.js
@@ -0,0 +1,165 @@
+class Rect{
+  constructor(x,y,z,dx,dy,dz,tex,wrap){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.dx = dx;
+    this.dy = dy;
+    this.dz = dz;
+    this.tex = tex;
+    this.wrap = wrap;
+
+    let geometry = new THREE.BoxGeometry(this.dx, this.dy, this.dz);
+    let material = [];
+    let texture;
+
+    for(let f=0; f<6; f++){
+
+      texture = new THREE.TextureLoader().load(`assets/images/${this.tex}.png`);
+      
+      texture.magFilter = THREE.NearestFilter;
+      texture.minFilter = THREE.LinearMipmapLinearFilter;
+      texture.wrapS = THREE.RepeatWrapping;
+      texture.wrapT = THREE.RepeatWrapping;
+      if(f === 0 || f === 1){
+        texture.repeat.set( (this.dz / this.wrap), (this.dy / this.wrap) );
+      }
+      if(f === 2 || f === 3){
+        texture.repeat.set( (this.dx / this.wrap), (this.dz / this.wrap) );
+      }
+      if(f === 4 || f === 5){
+        texture.repeat.set( (this.dx / this.wrap), (this.dy / this.wrap) );
+      }
+      
+      material.push(new THREE.MeshStandardMaterial( { map: texture, side: THREE.DoubleSide, shadowSide:THREE.FrontSide} ));
+    }
+
+    this.cube = new THREE.Mesh(geometry, material);
+    //this.cube.castShadow = true;
+    //this.cube.receiveShadow = true;
+    
+    this.cube.position.x = this.x;
+    this.cube.position.y = this.y;
+    this.cube.position.z = this.z;
+    
+    scene.add(this.cube);
+    
+  }
+
+  collideFloor(){
+    
+    let inside;
+    
+    if(player.x - halfWidth < this.x + (this.dx*0.5) && player.x + halfWidth > this.x - (this.dx*0.5) && player.z - halfWidth < this.z + (this.dz*0.5) && player.z + halfWidth > this.z - (this.dz*0.5)){
+      inside = true;
+    }
+
+    if(inside){
+      
+      if(player.y - halfHeight > this.y + (this.dy*0.5) && player.y - halfHeight + (player.yVel*deltaTime) < this.y + (this.dy*0.5)){
+        //above, but hit ground next frame
+        player.y = this.y + (this.dy*0.5) + halfHeight + 0.0000001;
+        player.yVel = 0;
+        player.onGround = true;
+      }
+
+      if(player.y + halfHeight < this.y - (this.dy*0.5) && player.y + halfHeight + (player.yVel*deltaTime) > this.y - (this.dy*0.5)){
+        //under, but hit head next frame
+        player.y = this.y - (this.dy*0.5) - halfHeight - 0.0000001;
+        player.yVel = 0;
+      }
+      
+    }
+    
+  }
+
+  collide(h){
+
+    let inY = false;
+    let canStep = false;
+    if(player.y - halfHeight + (player.yVel*deltaTime) < this.y + (this.dy*0.5) && player.y + halfHeight + (player.yVel*deltaTime) > this.y - (this.dy*0.5)){
+      inY = true;
+      if( (this.y + (this.dy*0.5)) - (player.y - halfHeight) <= stepHeight){
+        canStep = true;
+      }
+    }
+
+    if(inY){
+      
+      let inX = false;
+      let inXNext = false;
+      let inZ = false;
+      let inZNext = false;
+
+      if(player.x + halfWidth > this.x - (this.dx*0.5) && player.x - halfWidth < this.x + (this.dx*0.5)){
+        inX = true;
+      }
+      if(player.z + halfWidth > this.z - (this.dz*0.5) && player.z - halfWidth < this.z + (this.dz*0.5)){
+        inZ = true;
+      }
+      if(player.x + halfWidth + (player.xVel*deltaTime) > this.x - (this.dx*0.5) && player.x - halfWidth + (player.xVel*deltaTime) < this.x + (this.dx*0.5)){
+        inXNext = true;
+      }
+      if(player.z + halfWidth + (player.zVel*deltaTime) > this.z - (this.dz*0.5) && player.z - halfWidth + (player.zVel*deltaTime) < this.z + (this.dz*0.5)){
+        inZNext = true;
+      }
+
+      if(inZ && !inX && inXNext){
+        if(canStep && player.onGround){
+          player.y = this.y + (this.dy*0.5) + halfHeight + 0.0000001;
+        } else {
+          if(player.x < this.x){
+            player.x = this.x - (this.dx*0.5) - halfWidth;
+            player.xVel = 0;
+          }
+          if(player.x > this.x){
+            player.x = this.x + (this.dx*0.5) + halfWidth;
+            player.xVel = 0;
+          }
+        }
+      }
+
+      if(inX && !inZ && inZNext){
+        if(canStep && player.onGround){
+          player.y = this.y + (this.dy*0.5) + halfHeight + 0.0000001;
+        } else {
+          if(player.z < this.z){
+            player.z = this.z - (this.dz*0.5) - halfWidth;
+            player.zVel = 0;
+          }
+          if(player.z > this.z){
+            player.z = this.z + (this.dz*0.5) + halfWidth;
+            player.zVel = 0;
+          }
+        }
+      }
+
+      //bugfix
+      if(!inX && !inZ && inXNext && inZNext){
+        if(Math.abs(player.xVel) > Math.abs(player.zVel)){
+          if(player.z < this.z){
+            player.z = this.z - (this.dz*0.5) - halfWidth;
+            player.zVel = 0;
+          }
+          if(player.z > this.z){
+            player.z = this.z + (this.dz*0.5) + halfWidth;
+            player.zVel = 0;
+          }
+        } else {
+          if(player.x < this.x){
+            player.x = this.x - (this.dx*0.5) - halfWidth;
+            player.xVel = 0;
+          }
+          if(player.x > this.x){
+            player.x = this.x + (this.dx*0.5) + halfWidth;
+            player.xVel = 0;
+          }
+        }
+      }
+      
+    }
+    
+    
+  }
+  
+}
---
+++ js stuff/test/script.js
@@ -0,0 +1,180 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 120, window.innerWidth / window.innerHeight, 0.01, 80 );
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.autoUpdate = false;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild( renderer.domElement );
+
+scene.background = new THREE.Color( 0xa0c8ff );
+
+const ambientLight = new THREE.AmbientLight( 0xffffff, 0.8 );
+scene.add( ambientLight );
+
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
+
+const playerHeight = 1.8;
+const playerWidth = playerHeight * 0.3;
+const halfHeight = playerHeight * 0.5;
+const halfWidth = playerWidth * 0.5;
+const stepHeight = 0.6;
+
+const speed = 0.00007;
+const gravity = 0.000024;
+const jumpHeight = 0.009;
+const dampening = 0.012;
+
+var player = {
+  x: 0,
+  y: 0,
+  z: 0,
+  xVel: 0,
+  yVel: 0,
+  zVel: 0,
+  r: 0,
+  t: 0,
+  onGround: false
+}
+
+var blocks = [];
+
+window.onload = function(e){
+    blocks.push(new Rect(0,-10,0,20,1,20,"grass",1));
+}
+
+/*
+function setup(){
+  //var cnv = createCanvas(window.innerWidth, window.innerHeight);
+  //cnv.position(0,0);
+  //pixelDensity(1);
+  //noSmooth();
+  //frameRate(9999999);
+
+  //console.log("aa")
+  blocks.push(new Rect(0,-10,0,20,1,20,"grass",1));
+}
+*/
+
+function draw(){
+
+  if(deltaTime > 60){
+    deltaTime = 60;
+  }
+
+  switch (-keyIsDown(87) + keyIsDown(83) + (keyIsDown(65) * 10) + -(keyIsDown(68) * 10) + 11) {
+    case 11://no
+      break;
+    case 10://W
+      player.zVel -= (Math.cos(player.r) * speed) * deltaTime;
+      player.xVel -= (Math.sin(player.r) * speed) * deltaTime;
+      break;
+    case 20://WD
+      player.zVel -= (Math.cos(player.r + (PI * 0.25)) * speed) * deltaTime;
+      player.xVel -= (Math.sin(player.r + (PI * 0.25)) * speed) * deltaTime;
+      break;
+    case 21://D
+      player.zVel -= (Math.cos(player.r + (PI * 0.5)) * speed) * deltaTime;
+      player.xVel -= (Math.sin(player.r + (PI * 0.5)) * speed) * deltaTime;
+      break;
+    case 22://SD
+      player.zVel -= (Math.cos(player.r + (PI * 0.75)) * speed) * deltaTime;
+      player.xVel -= (Math.sin(player.r + (PI * 0.75)) * speed) * deltaTime;
+      break;
+    case 12://S
+      player.zVel -= (Math.cos(player.r + (PI)) * speed) * deltaTime;
+      player.xVel -= (Math.sin(player.r + (PI)) * speed) * deltaTime;
+      break;
+    case 2://SA
+      player.zVel -= (Math.cos(player.r + (PI * 1.25)) * speed) * deltaTime;
+      player.xVel -= (Math.sin(player.r + (PI * 1.25)) * speed) * deltaTime;
+      break;
+    case 1://A
+      player.zVel -= (Math.cos(player.r + (PI * 1.5)) * speed) * deltaTime;
+      player.xVel -= (Math.sin(player.r + (PI * 1.5)) * speed) * deltaTime;
+      break;
+    case 0://WA
+      player.zVel -= (Math.cos(player.r + (PI * 1.75)) * speed) * deltaTime;
+      player.xVel -= (Math.sin(player.r + (PI * 1.75)) * speed) * deltaTime;
+      break;
+  }
+
+  camera.rotateX(-player.t);
+  camera.rotateY(-player.r);
+
+  let rotateCam = (round(-movedX, 5) * 0.003);
+  let tiltCam = (round(movedY, 5) * 0.003);
+
+  player.r += (rotateCam*deltaTime)/8;
+  player.t -= (tiltCam*deltaTime)/8;
+
+  if (player.t >= 1.45) {
+    player.t = 1.45;
+  } else if (player.t <= -1.45) {
+    player.t = -1.45;
+  }
+
+  if(player.r > Math.PI){
+    player.r -= Math.PI*2;
+  } else if(player.r < -Math.PI){
+    player.r += Math.PI*2;
+  }
+
+  camera.rotateY(player.r);
+  camera.rotateX(player.t);
+
+  if(keyIsDown(32) && player.onGround){
+    player.yVel += jumpHeight;
+    player.onGround = false;
+  }
+
+  player.onGround = false;
+  for(let i=0; i<blocks.length; i++){
+    blocks[i].collideFloor();
+  }
+  for(let i=0; i<blocks.length; i++){
+    blocks[i].collide(i);
+  }
+
+  if(player.xVel != 0){
+    player.x += (player.xVel) * deltaTime;
+  }
+  if(player.zVel != 0){
+    player.z += (player.zVel) * deltaTime;
+  }
+  if(player.yVel != 0){
+    player.y += (player.yVel) * deltaTime;
+  }
+
+  player.xVel = lerp(player.xVel,0,(deltaTime*dampening));
+  player.zVel = lerp(player.zVel,0,(deltaTime*dampening));
+  
+  if(!isNaN(gravity * deltaTime)){
+    if(Math.abs(player.yVel - (gravity * deltaTime)) <= 0.000005){
+      player.yVel = 0;
+    } else if(Math.abs(gravity * deltaTime) > 0.000006){
+      player.yVel -= gravity * deltaTime;
+    }
+  }
+
+  camera.position.x = player.x;
+  camera.position.y = player.y + (halfHeight/2);
+  camera.position.z = player.z;
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  
+}
+
+function windowResized() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function mousePressed(){
+  requestPointerLock();
+}
