2c212a2a3467fe6046db93a16169c5b269314cdb
Landon Schroering <88072024+DaRealLando123@users.noreply.github.com>
2024-05-08 12:01:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const buttons = document.querySelectorAll('.sliding-button');
 buttons.forEach(button => {
   button.addEventListener('mouseenter', () => {
     button.style.transform = 'translateX(100px)';
+    let temp = document.getElementById("title");
+    temp.innerText = button.innerText;
     switch(button.innerText){
       case "Cube Cavern":
-        let temp = document.getElementById("title");
-        temp.innerText = button.innerText;
         break:
     }
   });
