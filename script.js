62db4aa304e9db1ef315a806de9e3dc81d4615b7
Landon Schroering <88072024+DaRealLando123@users.noreply.github.com>
2024-05-08 13:00:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
       button.style.transform = 'translateX(100px)';
       let temp = document.getElementById("title");
       temp.innerText = button.innerText;
-      switch(button.innerText){
-        case "Cube Cavern":
-          break;
-      }
     });
 
     button.addEventListener('mouseleave', () => {
