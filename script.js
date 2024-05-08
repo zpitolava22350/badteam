da4c8688a7eb61d5a41e823b6ac86a291871a386
Landon Schroering <88072024+DaRealLando123@users.noreply.github.com>
2024-05-08 12:08:35 -0400

    loads when HTML is FULLY loaded FIRST

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const buttons = document.querySelectorAll('.sliding-button');
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.sliding-button');
 
-buttons.forEach(button => {
-  button.addEventListener('mouseenter', () => {
-    button.style.transform = 'translateX(100px)';
-    let temp = document.getElementById("title");
-    temp.innerText = button.innerText;
-    switch(button.innerText){
-      case "Cube Cavern":
-        break;
-    }
-  });
+  buttons.forEach(button => {
+    button.addEventListener('mouseenter', () => {
+      button.style.transform = 'translateX(100px)';
+      let temp = document.getElementById("title");
+      temp.innerText = button.innerText;
+      switch(button.innerText){
+        case "Cube Cavern":
+          break;
+      }
+    });
 
-  button.addEventListener('mouseleave', () => {
-    button.style.transform = 'translateX(0)';
+    button.addEventListener('mouseleave', () => {
+      button.style.transform = 'translateX(0)';
+    });
   });
 });
