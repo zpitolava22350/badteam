295ed1b9ac8f45cd3c91f703f8f42dee0c2a3806
Landon Schroering <88072024+DaRealLando123@users.noreply.github.com>
2024-05-08 21:30:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const buttons = document.querySelectorAll('.sliding-button');
+const buttons = document.querySelectorAll('.sliding-button');
 
-  buttons.forEach(button => {
-    button.addEventListener('mouseenter', () => {
-      button.style.transform = 'translateX(100px)';
-      let temp = document.getElementById("title");
-      console.log(button.innerText);
-      console.log(temp.innerText);
-      temp.innerText = button.innerText;
-    });
+buttons.forEach(button => {
+  button.addEventListener('mouseenter', () => {
+    button.style.transform = 'translateX(100px)';
+    let temp = document.getElementById("title");
+    console.log(button.innerText);
+    console.log(temp.innerText);
+    temp.innerText = button.innerText;
+  });
 
-    button.addEventListener('mouseleave', () => {
-      button.style.transform = 'translateX(0)';
-    });
+  button.addEventListener('mouseleave', () => {
+    button.style.transform = 'translateX(0)';
   });
 });
