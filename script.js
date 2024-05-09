3915324dd300e6e3d37ed470963e59560a6e3fd4
zpitolava22350 <zpitolava22350@gmail.com>
2024-05-08 20:28:42 -0600

    sex

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const buttons = document.querySelectorAll('.sliding-button');
 
-document.addEventListener("DOMContentLoaded", function() {
+window.onload = function(e){
   buttons.forEach(button => {
-    console.log('Button:', button);
     button.addEventListener('mouseenter', () => {
-      button.style.transform = 'translateX(100px)';
-      document.getElementById("title").innerText = document.querySelector("button").innerText
-    });
-  
-    button.addEventListener('mouseleave', () => {
-      button.style.transform = 'translateX(0)';
+      document.getElementById("title").innerText = button.innerText;
     });
   });
-});
+};
 
 console.log("Loaded!");
