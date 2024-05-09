4b1f7a43ab758fbc89073c58d299c1758afa8d24
Landon Schroering <88072024+DaRealLando123@users.noreply.github.com>
2024-05-08 22:33:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ window.onload = function(e){
   buttons.forEach(button => {
     button.addEventListener('mouseenter', () => {
       document.getElementById("title").innerText = button.innerText;
+      var description = document.getElementById("description");
+      switch(button.innerText){
+        case "Cube Cavern":
+          description.innerText = "a remake of a roblox game lmao";
+          break;
+        case "GitHub Pages":
+          description.innerText = "where you are now";
+          break;
+      }
     });
   });
 };
