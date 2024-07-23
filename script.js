0748c7aeb05719148d7fb8b202c352e8e1ab66b6
zpitolava22350 <zpitolava22350@gmail.com>
2024-07-23 08:16:18 -0600

    AAAAAAAAAA

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ window.onload = function(e){
       document.getElementById("title").innerText = button.innerText;
       var description = document.getElementById("description");
       switch(button.innerText){
+        case "Minecraft 2 lol":
+          description.innerText = "Chunk based voxel engine + breaking blocks";
+          break;
+        case "Build to Survive":
+          description.innerText = "unfinished base building game";
+          break;
         case "Cube Cavern":
-          description.innerText = "a remake of a roblox game lmao";
+          description.innerText = "a remake of a roblox game lmao (not done lmfao)";
           break;
         case "GitHub Pages":
           description.innerText = "where you are now";
